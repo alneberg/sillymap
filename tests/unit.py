ffff6e9f6c505176af91b659b4e8bbb676352e60
Johannes Alneberg <johannesalneberg@gmail.com>
2017-01-10 12:29:50 +0100

    Updated tests for a count lookup

--- tests/unit.py
+++ tests/unit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import unittest
 from sillymap.burrows_wheeler import burrows_wheeler
+from sillymap.count_lookup import count_lookup
 
 class TestBW(unittest.TestCase):
     def test_basic_bw(self):
@@ -8,6 +9,19 @@ class TestBW(unittest.TestCase):
 
     def test_empty(self):
         self.assertEqual(burrows_wheeler(""), "$")
+
+class TestCountLookup(unittest.TestCase):
+    def test_basic_count_lookup(self):
+        c = count_lookup("mi")
+        self.assertEqual(c["i"], 0)
+        self.assertEqual(c["m"], 1)
+
+    def test_basic_count_lookup(self):
+        c = count_lookup("mississippi")
+        self.assertEqual(c['i'], 0)
+        self.assertEqual(c['m'], 4)
+        self.assertEqual(c['p'], 5)
+        self.assertEqual(c['s'], 7)
 
 if __name__ == '__main__':
     unittest.main()
