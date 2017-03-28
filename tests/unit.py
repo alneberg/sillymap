c5e0e36e43e7d00cc85a06fb428d08dbc5e24b6c
Johannes Alneberg <johannesalneberg@gmail.com>
2017-03-28 16:08:39 +0200

    Tests for rank class

--- tests/unit.py
+++ tests/unit.py
@@ -2,6 +2,7 @@
 import unittest
 from sillymap.burrows_wheeler import burrows_wheeler
 from sillymap.count_lookup import count_lookup
+from sillymap.rank_lookup import Rank
 
 class TestBW(unittest.TestCase):
     def test_basic_bw(self):
@@ -22,6 +23,15 @@ class TestCountLookup(unittest.TestCase):
         self.assertEqual(c['m'], 4)
         self.assertEqual(c['p'], 5)
         self.assertEqual(c['s'], 7)
+
+class TestRankLookup(unittest.TestCase):
+    def test_basic_rank_lookup(self):
+        rank = Rank()
+        rank.add_text("ipssm$pissii")
+
+        self.assertEqual(rank.rank(-1,"i"), 0)
+        self.assertEqual(rank.rank(0,"i"), 1)
+        self.assertEqual(rank.rank(11,"s"), 4)
 
 if __name__ == '__main__':
     unittest.main()
