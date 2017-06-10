494eff10b94b8583139b962a36064a9febedc7fd
Johannes Alneberg <johannesalneberg@gmail.com>
2017-06-10 18:13:57 +0200

    Seemingly less memory used by burrows wheeler transfor creation

--- sillymap/burrows_wheeler.py
+++ sillymap/burrows_wheeler.py
@@ -1,3 +1,20 @@
+class SubString(object):
+    # Class written by @MSeifert:
+    # https://stackoverflow.com/a/41669542
+    # slots reduce the memory overhead of the instances
+    __slots__ = ('str_', 'idx')
+
+    def __init__(self, str_, idx):
+        self.str_ = str_
+        self.idx = idx
+
+    # sorted only requires "<" to be implemented so we only need __lt__
+    def __lt__(self, other):
+        # temporarly create explicit substrings but only temporaries for the comparison
+        return self.str_[self.idx:] + self.str_[:self.idx] < other.str_[other.idx:] + other.str_[:other.idx]
+
+    def __repr__(self):
+        return self.idx
 
 def burrows_wheeler(text):
     """Calculates the burrows wheeler transform of <text>.
@@ -6,16 +23,12 @@ def burrows_wheeler(text):
     The text is assumed to not contain the character $"""
 
     text += "$"
-    all_permutations = []
-    for i in range(len(text)):
-        all_permutations.append((text[i:] + text[:i],i))
 
-    all_permutations.sort()
-    bw_l = [] # burrows wheeler as list
-    sa_i = [] # suffix array indices
+    # suffix arrau indices
+    sa_i = list(range(len(text)))
+    sa_i.sort(key=lambda i: SubString(text, i))
 
-    for w,j in all_permutations:
-        bw_l.append(w[-1])
-        sa_i.append(j)
+    # burrows wheeler as list
+    bw_l = [text[i-1] for i in sa_i]
 
     return "".join(bw_l), sa_i
