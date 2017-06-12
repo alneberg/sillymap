c288872eee40d8b56c48a468badcdb976790b7aa
Johannes Alneberg <johannesalneberg@gmail.com>
2017-06-12 11:22:55 +0200

    Slight speed optimization of the substring sorting for indexing

--- sillymap/burrows_wheeler.py
+++ sillymap/burrows_wheeler.py
@@ -2,15 +2,23 @@ class SubString(object):
     # Class written by @MSeifert:
     # https://stackoverflow.com/a/41669542
     # slots reduce the memory overhead of the instances
-    __slots__ = ('str_', 'idx')
+    __slots__ = ('str_', 'idx', 'str_len')
 
-    def __init__(self, str_, idx):
+    def __init__(self, str_, idx, str_len):
         self.str_ = str_
         self.idx = idx
+        self.str_len = str_len
 
     # sorted only requires "<" to be implemented so we only need __lt__
     def __lt__(self, other):
         # temporarly create explicit substrings but only temporaries for the comparison
+        #Optimized case to speed things up
+        if self.idx + 10 < self.str_len and other.idx + 10 < self.str_len:
+            if self.str_[self.idx:self.idx+10] < other.str_[other.idx:other.idx+10]:
+                return True
+            elif self.str_[self.idx:self.idx+10] > other.str_[other.idx:other.idx+10]:
+                return False
+        # this is the general case
         return self.str_[self.idx:] + self.str_[:self.idx] < other.str_[other.idx:] + other.str_[:other.idx]
 
     def __repr__(self):
@@ -24,9 +32,10 @@ def burrows_wheeler(text):
 
     text += "$"
 
+    text_len = len(text)
     # suffix arrau indices
-    sa_i = list(range(len(text)))
-    sa_i.sort(key=lambda i: SubString(text, i))
+    sa_i = list(range(text_len))
+    sa_i.sort(key=lambda i: SubString(text, i, text_len))
 
     # burrows wheeler as list
     bw_l = [text[i-1] for i in sa_i]
